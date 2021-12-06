47a36996e8cb1cdb660e9a3671ca81a9637fb7d7
Michelle Hlcn <83108919+Michellehlcn@users.noreply.github.com>
2021-12-07 09:32:42 +1100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ API_URL = 'https://paper-api.alpaca.markets'
 API_KEY = 'PKCH279SVWHRZZIN94KZ'
 SECRET_KEY = 'G5NFenVXXcdF0DmTaXENhVLsbfLuV24B5Ghvgbdf'
 
-DB_FILE = '/Users/michelle/Desktop/API/StockTrading/app.db'
+DB_FILE = 'app.db'
